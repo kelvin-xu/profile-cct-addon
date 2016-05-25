cd3b544ba374eb1e31e745a69e486f34c161aa8b
shaffiq <shaffiq.rahemtulla@ubc.ca>
2016-05-25 15:56:58 -0700

    Initial Cleanup
    phpcs

--- fields/ao-publications.php
+++ fields/ao-publications.php
@@ -1,5 +1,5 @@
 <?php
-Class Profile_CCT_AOPublications extends Profile_CCT_Field {
+class Profile_CCT_AOPublications extends Profile_CCT_Field {
 	var $default_options = array(
 		'type'          => 'aopublications',
 		'label'         => 'aopublications',
@@ -13,35 +13,31 @@ Class Profile_CCT_AOPublications extends Profile_CCT_Field {
 		'empty'         => '',
 		'after'         => '',
 	);
-	
 	var $shell = array(
 		'class' => 'aopublications',
 	);
-	
 	function field() {
 		$profile = Profile_CCT::get_object();
 		$pubtax = $profile->settings['archive']['ao_use_tax'][0];
 		$pubtaxall = $profile->settings['archive']['ao_use_taxall'][0];
-		$this->display_shell( array( 'class' => 'handle') );
-		//Add formatted publication here!! (readonly)
+		$this->display_shell( array( 'class' => 'handle' ) );
 		$this->display();
 		$this->display_end_shell();
-		$this->display_shell( array( 'class' => 'content-wrap') );
+		$this->display_shell( array( 'class' => 'content-wrap' ) );
 		global $post;
-$dataarray = maybe_unserialize(get_post_meta($post->ID,'profile_cct'));
-		//######## Need to check if setting "Publication Taxonomy" selected (only one) and grab taxonomy
+		$dataarray = maybe_unserialize( get_post_meta( $post->ID,'profile_cct' ) );
 		$terms_array = array();
-		$terms = wp_get_post_terms($post->ID, $pubtax, array("fields" => "slugs"));
-		if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
-    			foreach ( $terms as $term ) {
-        			$terms_array[] =  $term;
-    			}
+		$terms = wp_get_post_terms( $post->ID, $pubtax, array( 'fields' => 'slugs' ) );
+		if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) {
+			foreach ( $terms as $term ) {
+				$terms_array[] = $term;
+			}
 		}
 		$this->input_text( array(
 			'field_id' => 'aopublication-authors',
 			'label'    => 'Author(s)',
 			'size'     => 23,
-			'value'    => ( ! empty( $this->data['aopublication-authors'] ) ? $this->data['aopublication-authors'] : $dataarray[0][name][first] .' '.$dataarray[0][name][middle].' '.$dataarray[0][name][last] ),
+			'value'    => ( ! empty( $this->data['aopublication-authors'] ) ? $this->data['aopublication-authors'] : $dataarray[0][ name ][ first ] .' '.$dataarray[0][ name ][ middle ].' '.$dataarray[0][ name ][ last ] ),
 		) );
 
 		$this->input_text( array(
@@ -69,22 +65,26 @@ $dataarray = maybe_unserialize(get_post_meta($post->ID,'profile_cct'));
 			'all_fields' => $this->list_of_years( 20, -20 ),
 			'value'      => ( ! empty( $this->data['aopublication-year'] ) ? $this->data['aopublication-year'] : '2015' ),
 		) );
+
 		$this->input_text( array(
 			'field_id' => 'aopublication-publisher',
 			'label'    => 'Publisher',
 			'hidden'   => true,
 			'size'     => 23,
 		) );
+
 		$this->input_text( array(
 			'field_id' => 'aopublication-pagenumbers',
 			'label'    => 'Pages',
 			'size'     => 5,
 		) );
+
 		$this->input_text( array(
 			'field_id' => 'aopublication-doi',
 			'label'    => 'DOI',
 			'size'     => 5,
 		) );
+
 		$this->input_text( array(
 			'field_id' => 'aopublication-website',
 			'label'    => 'Website - http://{value}',
@@ -97,70 +97,62 @@ $dataarray = maybe_unserialize(get_post_meta($post->ID,'profile_cct'));
 			'size'     => 23,
 		) );
 
-		/*$this->input_select( array(
-			'field_id'   => 'start-date-month',
-			'label'      => 'Start Month',
-			'all_fields' => $this->list_of_months()
-		) );*/
-
 		$this->input_select( array(
 			'field_id'   => 'aopublication-status',
 			'label'      => 'Status',
 			'all_fields' => $this->aopublication_status(),
 		) );
-		//########
+
 		$this->input_textarea( array(
 			'field_id' => 'aopublication-abstract',
 			'label'    => 'Abstract',
 			'size'     => 35,
 		) );
+
 		$this->input_multiple( array(
 			'field_id'   => 'aopublication-book',
 			'class'	     => 'pub-book-class',
 			'selected_fields' => $this->data['aopublication-book'],
 			'all_fields' => $this->aopublication_book(),
 		) );
-      		$this->input_multiple( array(
+
+		$this->input_multiple( array(
 			'field_id'        => 'terms',
 			'selected_fields' => $this->data['terms'],
 			'all_fields'      => $terms_array,
 		) );
 
 		$themes_array = array();
-		$themes = get_terms( $pubtaxall, array('hide_empty' => false) );
-		if ( ! empty( $themes ) && ! is_wp_error( $themes ) ){
-    			foreach ( $themes as $theme ) {
-        			$themes_array[] =  $theme->slug;
-    			}
+		$themes = get_terms( $pubtaxall, array( 'hide_empty' => false ) );
+		if ( ! empty( $themes ) && ! is_wp_error( $themes ) ) {
+			foreach ( $themes as $theme ) {
+				$themes_array[] = $theme->slug;
+			}
 		}
-      		$this->input_multiple( array(
+		$this->input_multiple( array(
 			'field_id'        => 'themes',
 			'selected_fields' => $this->data['themes'],
 			'all_fields'      => $themes_array,
 		) );
 		$this->display_end_shell();
 	}
-	
+
 	function display() {
 		global $post;
-		$dataarray = maybe_unserialize(get_post_meta($post->ID,'profile_cct'));
-        $themeclasses = ($this->data['themes']) ? implode(' ',$this->data['themes']) : '';  
-        $termclasses = ($this->data['terms']) ? implode(' ',$this->data['terms']) : '';  
-      	$this->display_shell( array( 'class' => 'pubrec publication ' . $termclasses.' '.$themeclasses.' '.$hideclasses ) );
-		$separator = "";
-
-		//$this->display_shell( array( 'class' => 'thumbnail' ) );
-		//$this->display_end_shell();
+		$dataarray = maybe_unserialize( get_post_meta( $post->ID, 'profile_cct' ) );
+		$themeclasses = ( $this->data['themes']) ? implode( ' ',$this->data['themes'] ) : '';
+		$termclasses = ( $this->data['terms']) ? implode( ' ',$this->data['terms'] ) : '';
+		$this->display_shell( array( 'class' => 'pubrec publication ' . $termclasses.' '.$themeclasses.' '.$hideclasses ) );
+		$separator = '';
 
 		$this->display_text( array(
 					'class'        => 'thumbnail-holder',
 					'default_text' => '',
-					//'separator'    => $separator,
 					'value'       	 => ( ! empty( $this->data['aopublication-image'] ) ? '<span class="thumbnail"></span>' : '<span class="thumbnail"></span>' ),
 		) );
 
-		if ($this->data['book']) {
-			foreach( $this->data['book'] as $booktype ):
+		if ( $this->data['book'] ) {
+			foreach ( $this->data['book'] as $booktype ) :
 				$this->display_text( array(
 					'class'        => 'book',
 					'default_text' => '',
@@ -169,13 +161,13 @@ $dataarray = maybe_unserialize(get_post_meta($post->ID,'profile_cct'));
 				) );
 				$separator = ', ';
 			endforeach;
-        }
+		}
 
 		$this->display_text( array(
 			'field_id'       => 'aopublication-authors',
 			'class'          => 'aopublication-authors',
 			'default_text'   => 'Bruce Wayne',
-			'value'       	 => ( ! empty( $this->data['aopublication-authors'] ) ? $this->data['aopublication-authors'] : $dataarray[0][name][first] .' '.$dataarray[0][name][middle].' '.$dataarray[0][name][last] ),
+			'value'       	 => ( ! empty( $this->data['aopublication-authors'] ) ? $this->data['aopublication-authors'] : $dataarray[0][ name ][ first ] .' '.$dataarray[0][ name ][ middle ].' '.$dataarray[0][ name ][ last ] ),
 			'post_separator' => ' ',
 			'tag'            => 'strong',
 		) );
@@ -196,9 +188,6 @@ $dataarray = maybe_unserialize(get_post_meta($post->ID,'profile_cct'));
 			'class'          => 'aopublication-bookeds',
 			'default_text'   => 'Book Editors',
 			'value'       	 => ( ! ( ' ' == $this->data['aopublication-bookeds'] )  ? '' . $this->data['aopublication-bookeds'] : ' ' ),
-			//'filter_text'	 => ' ',
-			//'post_separator' => ' ',
-			//'tag'            => 'strong',
 		) );
 
 		$this->display_link( array(
@@ -208,7 +197,6 @@ $dataarray = maybe_unserialize(get_post_meta($post->ID,'profile_cct'));
 			'value'	       => ( ! empty( $this->data['aopublication-image'] ) ? $this->data['aopublication-image'] : ' ' ),
 			'maybe_link'   => true,
 			'tag'          => ( 'span' ),
-			//'href'         => ( ! empty( $this->data['aopublication-website'] ) ? $this->data['aopublication-website'] : '' ),
 			'post_separator' => ' ',
 		) );
 
@@ -218,22 +206,14 @@ $dataarray = maybe_unserialize(get_post_meta($post->ID,'profile_cct'));
 			'default_text'   => 'Journal/Book Title',
 			'value'       	 => ( ! empty( $this->data['aopublication-chapter'] ) ? $this->data['aopublication-chapter'] : ' ' ),
 			'post_separator' => ' ',
-			//'tag'            => 'strong',
 		) );
 
-		/*$this->display_text( array(
-			'field_id'       => 'start-date-month',
-			'class'          => 'start-date-month',
-			'default_text'   => 'January',
-			'post_separator' => ', ',
-		) );*/
-		$this->display_shell( array( 'class' => 'year-publisher') );
+		$this->display_shell( array( 'class' => 'year-publisher' ) );
 		$this->display_text( array(
 			'field_id'     => 'aopublication-year',
 			'class'        => 'aopublication-year',
 			'default_text' => '2015',
 			'value'       	 => ( ! empty( $this->data['aopublication-year'] ) ? $this->data['aopublication-year'] : '2015' ),
-			//'post_separator' => ' ',
 		) );
 
 		$this->display_text( array(
@@ -241,9 +221,6 @@ $dataarray = maybe_unserialize(get_post_meta($post->ID,'profile_cct'));
 			'class'          => 'aopublication-publisher',
 			'default_text'   => 'Cambridge Press',
 			'value'       	 => ( ! empty( $this->data['aopublication-publisher'] ) ? $this->data['aopublication-publisher'] : ' ' ),
-			//'filter_text'	 => ' ',
-			//'post_separator' => ' ',
-			//'tag'            => 'strong',
 		) );
 
 		$this->display_end_shell();
@@ -252,9 +229,6 @@ $dataarray = maybe_unserialize(get_post_meta($post->ID,'profile_cct'));
 			'class'          => 'aopublication-pagenumbers',
 			'default_text'   => '40-50',
 			'value'       	 => ( ! ( ' ' == $this->data['aopublication-pagenumbers'] )  ? '' . $this->data['aopublication-pagenumbers'] : ' ' ),
-			//'filter_text'	 => ' ',
-			//'post_separator' => ' ',
-			//'tag'            => 'strong',
 		) );
 
 		$this->display_text( array(
@@ -263,17 +237,8 @@ $dataarray = maybe_unserialize(get_post_meta($post->ID,'profile_cct'));
 			'default_text'   => '',
 			'post_separator' => ' ',
 			'value'       	 => ( ! empty( $this->data['aopublication-doi'] ) ? $this->data['aopublication-doi'] : ' ' ),
-			//'filter_text'	 => ' ',
-			//'tag'            => 'strong',
 		) );
 
-		/*$this->display_text( array(
-			'field_id'       => 'aopublication-title',
-			'class'          => 'aopublication-title',
-			'default_text'   => 'Cure for Profiles',
-			'post_separator' => ' ',
-			'tag'            => 'strong',
-		) );*/
 		$this->display_text( array(
 			'field_id'     => 'aopublication-status',
 			'class'        => 'aopublication-status',
@@ -282,10 +247,10 @@ $dataarray = maybe_unserialize(get_post_meta($post->ID,'profile_cct'));
 			'value'       	 => ( ! empty( $this->data['aopublication-status'] ) ? $this->data['aopublication-status'] : ' ' ),
 			'post_separator' => ' ',
 		) );
-		//$this->display_shell( array( 'class' => 'aopublication-dates') );
+
 		$this->display_shell( array( 'class' => 'profileterms' ) );
-		if ( isset( $this->data['terms'] ) ):
-			foreach( $this->data['terms'] as $term ):
+		if ( isset( $this->data['terms'] ) ) :
+			foreach ( $this->data['terms'] as $term ) :
 				$this->display_text( array(
 					'class'        => 'terms',
 					'default_text' => 'my specialization',
@@ -301,7 +266,6 @@ $dataarray = maybe_unserialize(get_post_meta($post->ID,'profile_cct'));
 		$this->display_text( array(
 					'class'        => 'abstract-icon',
 					'default_text' => '',
-					//'separator'    => $separator,
 					'value'       	 => ( ! empty( $this->data['aopublication-abstract'] ) ? '<span style="display:inline-block" class="dashicons dashicons-plus-alt"></span>' : '<span style="display:none" class="dashicons dashicons-plus-alt"></span>' ),
 		) );
 
@@ -315,10 +279,10 @@ $dataarray = maybe_unserialize(get_post_meta($post->ID,'profile_cct'));
 		$this->display_end_shell();
 		$this->display_end_shell();
 	}
-	
-  	/**
+
+	  /**
 	 * publication_status function.
-	 * 
+	 *
 	 * @access public
 	 * @return void
 	 */
@@ -329,12 +293,12 @@ $dataarray = maybe_unserialize(get_post_meta($post->ID,'profile_cct'));
 	function aopublication_book() {
 		return array( 'Book' );
 	}
-  
+
 	public static function shell( $options, $data ) {
-		new Profile_CCT_AOPublications( $options, $data ); 
+		new Profile_CCT_AOPublications( $options, $data );
 	}
 }
 
 function profile_cct_aopublications_shell( $options, $data ) {
-	Profile_CCT_AOPublications::shell( $options, $data ); 
+	Profile_CCT_AOPublications::shell( $options, $data );
 }
